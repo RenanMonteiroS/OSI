83ab559f5e67ce9b12cf5ffbf6092e65d1df75f4
RenanMonteiroS <renanmonteirodes@gmail.com>
2025-01-04 18:20:27 -0300

    Code Improvement - User class/document with created_at and modified_at attributes/columns

--- model/user.py
+++ model/user.py
@@ -12,5 +12,13 @@ class User(Document):
     mfaSecret = StringField(required=True)
     status = StringField(required=True, choices=STATUS, default='inactive')
     role= StringField(required=True, choices=ROLES, default='common')
-    
+    created_at = DateTimeField(required=True, default=datetime.now())
+    modified_at = DateTimeField(required=True, default=datetime.now())
+
+    def save(self, *args, **kwargs):
+        if not self.created_at:
+            self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        return super(User, self).save(*args, **kwargs)
+
         
